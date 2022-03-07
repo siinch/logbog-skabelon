256f5d2fe7fdd9cc39e5c45eb6e89b7f3c3e0608
Simon Ingemann Bach <sic@slotshaven.dk>
2022-03-07 15:28:18 +0100

    reset new layout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let owner = "Kaj Børge";
 // Set title
 document.getElementsByTagName("title")[0]
-.innerHTML = "My Portfolio";
+.innerHTML = owner +"s Portfolio";
 
 // Navigation bar Javascript
 // find the topnav object
@@ -8,7 +9,7 @@ let topnav = document.getElementById("topnav");
 
 // define the tabs
 let tabs = [
-  {name: "Home", href: "index.html"},
+  {name: owner, href: "index.html", class: "home"},
   {name: "Projects", href: "projects.html"},
   {name: "Notes", href: "notes.html"}
 ];
@@ -18,14 +19,16 @@ let tabs = [
 for (let tab of tabs) {
   let newtab = document.createElement("a");
   newtab.innerHTML = tab.name;
-  newtab.href = tab.href; 
+  newtab.href = tab.href;
+  if(tab.class != undefined)
+    newtab.className = tab.class;
   topnav.appendChild(newtab);
 }
 
 // add responsiveness to the topnav
 let dropDownButton = document.createElement("a");
 href="javascript:void(0);"
-dropDownButton.className += " icon";
+dropDownButton.className += "icon";
 dropDownButton.onclick = collapseTopNav;
 dropDownButton.innerHTML = "<i class='fa fa-bars'></i>";
 topnav.appendChild(dropDownButton);
@@ -37,20 +40,3 @@ function collapseTopNav() {
       topnav.className = "topnav";
     }
   }
-
-// Accordion Javascript
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
