70af29ba4453a6951b145780d98bc277f5effc5d
siinch <simon.ingemanns@protonmail.com>
2022-03-08 21:16:40 +0100

    project cards shaking images and note cards + onpage navigation

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-let profileName = "Kaj Børge";
-// Set title
-document.getElementsByTagName("title")[0]
-.innerHTML = profileName +"s Portfolio";
-
 // Navigation bar Javascript
 // find the topnav object
 let topnav = document.getElementById("topnav");
-
-// define the tabs
-let tabs = [
-  {name: profileName, href: "index.html", class: "home"},
-  {name: "Projects", href: "projects.html"},
-  {name: "Notes", href: "notes.html"}
-];
-
-// create the tabs
-for (let tab of tabs) {
-  let newtab = document.createElement("a");
-  newtab.innerHTML = tab.name;
-  newtab.href = tab.href;
-  if(tab.class != undefined)
-    newtab.className = tab.class;
-  topnav.appendChild(newtab);
-}
 
 // add responsiveness to the topnav
 let dropDownButton = document.createElement("a");
@@ -54,7 +32,3 @@ function followTopNav() {
     topnav.classList.remove("sticky");
   }
 } 
-
-  // set the name
-  document.getElementById("profile-name")
-  .innerHTML = profileName;
