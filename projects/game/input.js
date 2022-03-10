fa61776309129ffce9c6df05e6c7c2c377c4f1aa
siinch <simon.ingemanns@protonmail.com>
2022-03-10 17:32:05 +0100

    add click and touch controls

--- projects/game/input.js
+++ projects/game/input.js
@@ -43,3 +43,44 @@ document.addEventListener("keyup", function(event) {
     }
 
 });
+
+// add click support
+if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i))
+    document.addEventListener("touchstart", handleClick);
+else
+    document.addEventListener("mousedown", handleClick);
+
+function handleClick() {
+    
+    // if the menu is clicked, start the game
+    if(gameState == "menu") {
+        if (music.paused) music.play();
+        gameState = "action";
+        birdCanFlap = true;
+        return; 
+    }
+
+    // flap the wings if the action scene is clicked
+    if (gameState == "action") {
+        if(birdYAccelleration == 0) birdYAccelleration = birdBeginningYAccelleration;
+        birdYSpeed = birdFlapForce;
+        birdFlapSound.currentTime = 0.1;
+        birdFlapSound.play();
+        return;
+    }
+
+    // reset the game if the gameover screen is clicked
+    if(gameState == "gameover") {
+        gameState = "menu";
+        birdYPosition = birdStartYPosition;
+        birdYSpeed = birdStartYSpeed;
+        birdYAccelleration = birdStartYAccelleration;
+        birdCanFlap = false;
+        fireballs = [];
+        fireballTimeSinceLastSpawn = fireballSpawnInterval;
+        scoreValue = 0;
+        coins = [];
+        return; 
+    }
+
+}
