31bd17ba977df18e351391fc3fed9ed2f1495a36
siinch <simon.ingemanns@protonmail.com>
2022-03-10 10:53:17 +0100

    simple file server

---
+++ server.js
@@ -0,0 +1,16 @@
+// import the neccessary libraries
+const express = require("express");
+const server = express();
+const port = 3000;
+
+// configure express server
+server.use(express.static("../"));
+
+// send the root index.html
+server.get("/", (request, response) => {
+  console.log("New user connecting. Loading site..");
+  response.sendFile("index.html");
+});
+
+// start server
+server.listen(port, () => console.log("Listening on port " + port));
