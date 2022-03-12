bee7619b83a3ee264401981959b0cf8fa2d224b3
siinch <simon.ingemanns@protonmail.com>
2022-03-12 17:23:33 +0100

    update tasks function

--- projects/webapp/script.js
+++ projects/webapp/script.js
@@ -1,20 +1,4 @@
-let tasks = [
-    {title: "Task #1", status: "backlog"},
-    {title: "Task #2", status: "to-do"},
-    {title: "Task #3", status: "doing"},
-    {title: "Task #4", status: "done"}
-];
-for (let task of tasks) {
-
-    // find the appropriate task list
-    let parent = document.getElementById(task.status);
-
-    // create task component
-    let taskComponent = createTaskComponent(parent, task);
-
-    // append task component
-    parent.appendChild(taskComponent);
-}
+let tasks = [];
 
 // add functionality to the addTaskButton
 let addTaskButton = document.getElementById("add-task-button");
@@ -22,24 +6,41 @@ addTaskButton.addEventListener("click", function() {
     if(!confirm("Add new task?")) 
         return;
     
-    let parent = document.getElementById("backlog");
-    let task = {title: prompt("Title of new task:")};
-    let taskComponent = createTaskComponent(parent, task);
-    backlog.appendChild(taskComponent);
+    tasks.push({
+        title: prompt("Title of new task:"),
+        status: "backlog" 
+    });
+
+    updateTasks();
 });
 
+function updateTasks() {
 
-function removeTaskComponent(parent, taskComponent) {
-    if(!confirm("Are you sure you wish to remove this task?"))
-        return;
-    
-    parent.removeChild(taskComponent)
+    // Remove all taskComponents
+    let taskComponents = document.getElementsByClassName("task");
 
+    for (let taskComponent of taskComponents) {
+        taskComponent.parentElement.removeChild(taskComponent);
+    }
+
+    // Insert new task components
+    for (let task of tasks) {
+
+        // find the appropriate task list
+        let parent = document.getElementById(task.status);
+
+        // create task component
+        let taskComponent = createTaskComponent(task);
+
+        // append task component
+        parent.appendChild(taskComponent);
+    }
 }
 
-function createTaskComponent (parent, task) {
+function createTaskComponent (task) {
     let taskComponent = document.createElement("div");
     taskComponent.classList.add("task");
+    taskComponent.task = task;
 
     // add title
     let title = document.createElement("h3");
@@ -50,7 +51,10 @@ function createTaskComponent (parent, task) {
     let remove = document.createElement("button");
     remove.innerHTML = "-";
     remove.addEventListener("click", function () {
-        removeTaskComponent(parent, taskComponent)
+        if(!confirm("Are you sure you wish to remove this task?"))
+            return;
+        tasks = tasks.remove(task);
+        updateTasks();
     });
     taskComponent.appendChild(remove);
 
@@ -65,4 +69,15 @@ function createTaskComponent (parent, task) {
     taskComponent.appendChild(shiftRight);
 
     return taskComponent;
+}
+
+Array.prototype.remove = function (elementToRemove) {
+    let result = [];
+    
+    for(let element of this) {
+        if(element != elementToRemove)
+            result.push(element);
+    }
+
+    return result;
 }
