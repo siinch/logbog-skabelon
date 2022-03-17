c718d4ee9652a2176fa59811f37c25f2ca291880
siinch <simon.ingemanns@protonmail.com>
2022-03-17 07:53:38 +0100

    install bcrypt and hash user password on signup

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/usermodel.js");
 
 const userController = express.Router();
 
 userController.post("/user/signup", async (request, response) => {
-  console.log("Signing up user...:" + JSON.stringify(request.body));
-  response.json(request.body);
+
+  let user = request.body;
+  console.log("Signing up user...:" + JSON.stringify(user.username));
+
+  // generate random salt between 20.000 and 30.000
+  user.salt = Math.random()*30000-10000;
+
+  // hash the password
+  user.hash = await bcrypt.hash(user.password, user.salt);
+
+  user = new User(user);
+
+  await user.save();
+
+  response.json({username: user.username});
 });
 
 userController.post("/user/login", async (request, response) => {
