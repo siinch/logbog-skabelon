5bbabe96f9f56a941c90e85279f1855ea9d80bb3
siinch <simon.ingemanns@protonmail.com>
2022-03-28 12:34:46 +0200

    login form working

--- client/projects/webapp/components/userloginform.js
+++ client/projects/webapp/components/userloginform.js
@@ -26,10 +26,29 @@ class UserLoginForm extends HTMLElement {
 
         let form = this.shadowRoot.getElementById("login");
         form.onsubmit = function () {
-            console.log(form.username.value, form.password.value);
+            // wrap function to avoid redirection
+            let handleForm = async function () {
+                let user = {
+                    username: form.username.value,
+                    password: form.password.value
+                }
+                
+                let response = await logInUser(user);
+
+                if(!response.ok) {
+                    alert (response.status + ": " + response.statusText);
+                    return;
+                }
+
+                let data = await response.json();
+                alert("Logged in user: " + JSON.stringify(data))
+                userSession = data;
+                reloadTasks();
+            }
+            handleForm();
             // return false to prevent redirection
             return false;
-        } 
+        }
     }
 
     connectedCallback () {
