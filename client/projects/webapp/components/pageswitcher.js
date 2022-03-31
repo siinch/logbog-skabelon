5e67f2794ff39d5eff6d8b67188ec9332d12ad10
siinch <simon.ingemanns@protonmail.com>
2022-03-31 07:51:33 +0200

    clean up page-switcher and add some comments

--- client/projects/webapp/components/pageswitcher.js
+++ client/projects/webapp/components/pageswitcher.js
@@ -1,15 +1,22 @@
+// The main component for switching between other components.
+// Effectively makes the app a single page application (SPA)
 class PageSwitcher extends HTMLElement {
 
     constructor () {
         super();
         this.attachShadow({mode: "open"});
+
+        // load the last page or start at the login form
         if(localStorage.page == undefined)
             this.current = "user-login-form";
         else
             this.current = localStorage.page;
+
+        // subscribe to the switch-page event
         Channel.subscribe(
             "switch-page",
             (data) => {
+                // switch the page and save the page state
                 this.current = data;
                 localStorage.setItem("page", data);
             }
@@ -17,37 +24,35 @@ class PageSwitcher extends HTMLElement {
     }
 
     render () {
-        
+        // insert the current component into the page
         this.shadowRoot.innerHTML = `
-        <style>
-        * {
-            width: 100%;
-        }
-        </style>
-
             <${this.current}></${this.current}>
         `;
     }
 
+    // call when getting "current" e.g. this.current
     get current () {
         return this.getAttribute("current");
     }
 
+    // call when setting "current" e.g. this.current = "task-board";
     set current (newValue) {
         this.setAttribute("current", newValue);
     }
 
+    // call when component is put into the page/DOM
     connectedCallback () {
         this.render();        
     }
 
+    // observe when the "current" attribute changes
     static get observedAttributes () {
         return ["current"];
     }
 
+    // call when one of the observed attribute change
     attributeChangedCallback (attribute, oldValue, newValue) {
         if(attribute == "current") this.render();
     }
 }
-
 customElements.define("page-switcher", PageSwitcher);
